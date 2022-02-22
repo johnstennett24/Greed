59474368827a97afdf0efbbfe60d5e896a557a46
alexghow903 <97628131+alexghow903@users.noreply.github.com>
2022-02-21 20:21:32 -0700

    Add files via upload

---
+++ Game/Directing/Director.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using Unit04.Game.Casting;
+using Unit04.Game.Services;
+
+
+namespace Greed
+{
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private KeyboardService keyboardService = null;
+        private VideoService videoService = null;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given KeyboardService and VideoService.
+        /// </summary>
+        /// <param name="keyboardService">The given KeyboardService.</param>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
+        {
+            this.keyboardService = keyboardService;
+            this.videoService = videoService;
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void StartGame(Cast cast)
+        {
+            videoService.OpenWindow();
+            while (videoService.IsWindowOpen())
+            {
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
+            }
+            videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Gets directional input from the keyboard and applies it to the robot.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void GetInputs(Cast cast)
+        {
+            Actor robot = cast.GetFirstActor("robot");
+            Point velocity = keyboardService.GetDirection();
+            robot.SetVelocity(velocity);
+        }
+
+        /// <summary>
+        /// Updates the robot's position and adds or removes points.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void DoUpdates(Cast cast)
+        {
+            int score = 0;
+            Actor banner = cast.GetFirstActor("banner");
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+
+            banner.SetText("Score: " + score);
+            int maxX = videoService.GetWidth();
+            int maxY = videoService.GetHeight();
+            robot.MoveNext(maxX, maxY);
+
+            foreach (Actor actor in Gem)
+            {
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    Artifact artifact = (Artifact) actor;
+                    score++;
+                }
+            } 
+            foreach (Actor actor in Rock)
+            {
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    Artifact artifact = (Artifact) actor;
+                    score--;
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Draws the actors on the screen.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void DoOutputs(Cast cast)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            videoService.ClearBuffer();
+            videoService.DrawActors(actors);
+            videoService.FlushBuffer();
+        }
+
+    }
+}
